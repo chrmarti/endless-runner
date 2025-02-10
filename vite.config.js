6771efa994b8eff3bf4f761ad38c98851d85deb5
Christof Marti <chrmarti@microsoft.com>
2025-02-10 15:55:12 +0100

    publish to GH pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  base: '/endless-runner/',
   publicDir: 'assets',
   server: {
     open: true,
@@ -10,6 +11,7 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
-    sourcemap: true
+    sourcemap: true,
+    copyPublicDir: true
   }
 });
